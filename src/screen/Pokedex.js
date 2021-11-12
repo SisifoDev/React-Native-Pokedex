2649126a275de3af567ae2f8e307fcad51865a57
Manuel Perdomo <manuelperdomo1710@icloud.com>
2021-11-12 11:46:51 -0500

    [Add] connection with the API

--- src/screen/Pokedex.js
+++ src/screen/Pokedex.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { getPokemonApi } from "../api/pokemon";
 
 export default function Pokedex() {
+  useEffect(() => {
+    (async () => {
+      await loadPokemons();
+    })();
+  }, []);
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonApi();
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <SafeAreaView>
       <Text>Pokedex</Text>
