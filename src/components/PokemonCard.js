c48edacff9a471158ccdbdee2bf137393105ce42
Manuel Perdomo <manuelperdomo1710@icloud.com>
2021-11-13 09:40:55 -0500

    [Add] PokemonCard component

---
+++ src/components/PokemonCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function PokemonCard(props) {
+  const { pokemon } = props;
+  return (
+    <View>
+      <Text>Pokemon Info</Text>
+    </View>
+  );
+}
